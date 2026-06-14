f8d626a1bc379e85f33bb3e7eef990684e4b3434
sakethverse-dev <sakethverse@gmail.com>
2026-06-14 10:32:03 +0530

    fix: disable allow_credentials in CORS to allow wildcard origins on Render

--- crewai_server.py
+++ crewai_server.py
@@ -173,7 +173,7 @@ def firecrawl_scraper_tool(url: str) -> str:
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
